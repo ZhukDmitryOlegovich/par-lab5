ccb1bbe91e86315e32ab490b6399d68ade881b29
ZhukDima <zhuk@gmail.com>
2021-12-10 22:42:46 +0300

    Scripted auto-commit on change (2021-12-10 22:42:46) by gitwatch.sh

--- src/main/java/ActorCache.java
+++ src/main/java/ActorCache.java
@@ -2,15 +2,25 @@ import akka.actor.AbstractActor;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ActorCache extends AbstractActor {
-    private final Map<String, Long> results = new HashMap<>();
+    private final Map<String, Long> cache = new HashMap<>();
 
     @Override
     public Receive createReceive() {
-        return receiveBuilder();
-//                .match(
-//
-//                );
+        return receiveBuilder()
+                .match(
+                        MessageGetResult.class,
+                        message -> sender().tell(
+                                Optional.ofNullable(cache.get(message.getUrl())),
+                                self()
+                        )
+                )
+                .match(
+                        MessageCacheResult.class,
+                        message -> cache.put(message.getUrl(), message.getResponseTime())
+                )
+                .build();
     }
 }
