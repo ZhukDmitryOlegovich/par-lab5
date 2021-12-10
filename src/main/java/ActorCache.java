518338e84900cd9a2850c286ead06e6d143e8c11
ZhukDima <zhuk@gmail.com>
2021-12-10 22:08:45 +0300

    Scripted auto-commit on change (2021-12-10 22:08:45) by gitwatch.sh

---
+++ src/main/java/ActorCache.java
@@ -0,0 +1,16 @@
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActorCache extends AbstractActor {
+    private final Map<String, Long> results = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder();
+//                .match(
+//
+//                );
+    }
+}
--- src/main/java/AverageHttpResponseTimeApp.java
+++ src/main/java/AverageHttpResponseTimeApp.java
@@ -1,6 +1,44 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class AverageHttpResponseTimeApp {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef actor = system.actorOf(Props.create(ActorCache.class));
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = null;// <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
+    private static Flow<HttpRequest, HttpResponse, NotUsed> flowHttpRequest(
+            ActorMaterializer materializer, ActorRef actor
+    ) {
+        return Flow.of(HttpRequest.class)
+
+    }
 }
